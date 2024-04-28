52685ce595c1f54104051de3571af55c5dbdb7ce
Diego B. <dbsmendes@gmail.com>
2024-04-28 06:27:17 -0300

    [BUILD] Create README and adjust test controller

--- ms-go/test/controller/application_test.go
+++ ms-go/test/controller/application_test.go
@@ -7,45 +7,26 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"github.com/likexian/gokit/assert"
 )
 
 func TestIndexHome(t *testing.T) {
-
-	// Build our expected body
-	body := gin.H{
+	expectedBody := map[string]interface{}{
 		"message": "[ms-go] | Success",
-		"status":  200,
+		"status":  float64(200),
 	}
 
-	// Grab our router
 	router := router.SetupRouter()
 
-	// Perform a GET request with that handler.
 	req, _ := http.NewRequest(http.MethodGet, "/", nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	// Assert we encoded correctly,
-
-	// the request gives a 200
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// Convert the correct object response to a map
-	response := struct {
-		Data struct {
-			Api struct {
-				Status  int    `json:"status"`
-				Message string `json:"message"`
-			} `json:"api"`
-		} `json:"data"`
-	}{}
-
-	err := json.Unmarshal(w.Body.Bytes(), &response)
-
-	// Make some assertions on the correctness of the response.
+	var responseBody map[string]interface{}
+	err := json.Unmarshal(w.Body.Bytes(), &responseBody)
 	assert.Nil(t, err)
-	assert.Equal(t, body["message"], response.Data.Api.Message)
 
+	assert.Equal(t, expectedBody, responseBody)
 }
